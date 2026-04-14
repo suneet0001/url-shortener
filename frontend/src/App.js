06ab75905292d8d8b13d211f3d269098389986eb
Suneet Bharangar <suneet3051@gmail.com>
2026-04-15 00:39:00 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/password/:short" element={<PasswordPage />} />
       </Routes>
     </BrowserRouter>
   );

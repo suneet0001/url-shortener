8a7190ceb93238977234ab4b9834b495559429e0
Suneet Bharangar <suneet3051@gmail.com>
2026-04-15 00:57:57 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,27 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import PasswordPage from "./PasswordPage";
+
+function Main() {
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const short = params.get("short");
+
+  return (
+    <Routes>
+      {short ? (
+        <Route path="/" element={<PasswordPage />} />
+      ) : (
+        <Route path="/" element={<Dashboard />} />
+      )}
+    </Routes>
+  );
+}
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-      </Routes>
+      <Main />
     </BrowserRouter>
   );
 }
